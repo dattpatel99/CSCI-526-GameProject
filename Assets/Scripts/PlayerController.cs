6b5e5f5e20c7a769799898a9c81661f1a136a346
eliashosseini <ehossein@usc.edu>
2023-06-03 15:35:54 -0700

    working on jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,52 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 10.0f;
+    public float landSpeed = 10.0f;
+    public float airSpeed = 5.0f;
+
+    Rigidbody2D rb;
+    float jumpAmount = 10;
 
     private float horizontalInput;
-    private float verticalInput;
+    private bool jumpInput;
+    private bool isJumping;
 
-    // Update is called once per frame
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+
+        isJumping = false;
+    }
+
     void Update()
     {
+        // Get Inputs
         horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
+        jumpInput = Input.GetButtonDown("Jump");
+
+        if (isJumping)
+        {
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * airSpeed);
+        }
+        else
+        {
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * landSpeed);
+        }
+        
+
+        if (!isJumping && jumpInput)
+        {
+            rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            Debug.Log("hit the floor");
+            isJumping = false;
+        }
     }
 }

711258bfd3450f1eee69edccf05c4c5dd9679335
Datt Patel <dhpatel1999@gmail.com>
2023-06-04 14:52:16 -0700

    Laser Color Change based on shot

--- Assets/Scripts/ShootMechanic.cs
+++ Assets/Scripts/ShootMechanic.cs
@@ -18,60 +18,47 @@ public class ShootMechanic : MonoBehaviour
     void Update()
     {
         Vector3 nozzlePosition = nozzle.transform.position;
-        if (Input.GetButtonDown("Fire1"))
+        bool take = Input.GetButtonDown("Fire1");
+        bool give = Input.GetButtonDown("Fire2");
+        if (take || give)
         {
-            TakeTime(nozzlePosition);
-        }
-        else if (Input.GetButtonDown("Fire2"))
-        {
-            GiveTime(nozzlePosition);
+            RaycastHit2D hit = Physics2D.Raycast(nozzle.transform.position, transform.TransformDirection(Vector2.right), laserLength);
+            // If no collider hit then show laser till laser length
+            AlterColor(laserLine, Color.yellow);
+            if (hit.collider != null)
+            {
+                if (take && hit.collider.gameObject.CompareTag("TimeObject") &&
+                    hit.collider.gameObject.GetComponent<TimeObject>().checkSubtration())
+                {
+                    hit.collider.gameObject.GetComponent<TimeObject>().SubtractTime(1);
+                    player.GetComponent<TimeBank>().AddTime(1);
+                    AlterColor(laserLine, Color.red);
+                }
+                else if (give && hit.collider.gameObject.CompareTag("TimeObject") &&
+                         player.GetComponent<TimeBank>().checkSubtract() &&
+                         hit.collider.gameObject.GetComponent<TimeObject>().checkAddition())
+                {
+                    hit.collider.gameObject.GetComponent<TimeObject>().AddTime(1);
+                    player.GetComponent<TimeBank>().SubtractTime(1);
+                    AlterColor(laserLine, Color.green);
+                }
+                laserLine.SetPosition(0, nozzlePosition);
+                laserLine.SetPosition(1, hit.point);
+                StartCoroutine(LaserShow());
+            }
+            else
+            {
+                laserLine.SetPosition(0, nozzlePosition);
+                laserLine.SetPosition(1, nozzlePosition + nozzle.transform.right * laserLength);
+                StartCoroutine(LaserShow());
+            }
         }
     }
 
-    private void TakeTime(Vector3 nozzlePos)
+    void AlterColor(LineRenderer line, Color color)
     {
-        RaycastHit2D hit = Physics2D.Raycast(nozzle.transform.position, transform.TransformDirection(Vector2.right), laserLength);
-        if(hit.collider != null)
-        {
-            if (hit.collider.gameObject.CompareTag("TimeObject") && hit.collider.gameObject.GetComponent<TimeObject>().checkSubtration())
-            {
-                laserLine.SetPosition(0, nozzlePos);
-                laserLine.SetPosition(1, hit.point);
-                hit.collider.gameObject.GetComponent<TimeObject>().SubtractTime(1);
-                player.GetComponent<TimeBank>().AddTime(1);
-                StartCoroutine(LaserShow());
-            }
-            // Uncomment if you want to show laser if not hitting anything
-            /*else
-            {
-                laserLine.SetPosition(0, nozzlePos);
-                laserLine.SetPosition(1, nozzlePos + nozzle.transform.right * laserLength);
-                StartCoroutine(LaserShow());
-            }*/
-        }
-    }
-
-    private void GiveTime(Vector3 nozzlePos)
-    {
-        RaycastHit2D hit = Physics2D.Raycast(nozzlePos, transform.TransformDirection(Vector2.right), laserLength);
-        if(hit.collider != null)
-        {
-            if (hit.collider.gameObject.CompareTag("TimeObject") && player.GetComponent<TimeBank>().checkSubtract() && hit.collider.gameObject.GetComponent<TimeObject>().checkAddition())
-            {
-                laserLine.SetPosition(0, nozzlePos);
-                laserLine.SetPosition(1, hit.point);
-                hit.collider.gameObject.GetComponent<TimeObject>().AddTime(1);
-                player.GetComponent<TimeBank>().SubtractTime(1);
-                StartCoroutine(LaserShow());
-            }
-            // Uncomment if you want to show laser if not hitting anything
-            /*else
-            {
-                laserLine.SetPosition(0, nozzlePos);
-                laserLine.SetPosition(1, nozzlePos + nozzle.transform.right * laserLength);
-                StartCoroutine(LaserShow());
-            }*/
-        }
+        line.startColor = color;
+        line.endColor = color;
     }
 
     IEnumerator LaserShow()

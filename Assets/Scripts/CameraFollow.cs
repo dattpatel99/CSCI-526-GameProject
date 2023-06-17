e786c1a2111a8b72adda70605233591d669663f3
eliashosseini <ehossein@usc.edu>
2023-06-16 17:02:47 -0700

    Camera follow update; added rewindable objects from templates

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
-    public float xOffset = 2;
+    public float xLimit = 5f;
+    public float yLimit = 5f;
+    public float cameraSpeed = 4f;
 
     public float zOffset = -10;
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x + xOffset, 0, zOffset);
-        
+        float xError = Mathf.Abs(player.transform.position.x - transform.position.x);
+        float yError = Mathf.Abs(player.transform.position.y - transform.position.y);
+
+        if (xError > xLimit || yError > yLimit)
+        {
+            float step = cameraSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, 0, zOffset), step);
+        }
     }
 }

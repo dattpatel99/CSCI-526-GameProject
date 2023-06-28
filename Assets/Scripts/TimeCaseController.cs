c748d175264ef19b2c99bcd818917899fa6a15b7
adam askari <34635255+bearownage@users.noreply.github.com>
2023-06-28 13:39:41 -0700

    Got it opening

--- Assets/Scripts/TimeCaseController.cs
+++ Assets/Scripts/TimeCaseController.cs
@@ -5,7 +5,10 @@ using TMPro;
 
 public class TimeCaseController : TimeObject
 {
+    public GameObject timeCasePivotPoint;
     public GameObject timeCaseParent;
+
+    private float rotationSpeed = 50.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +22,13 @@ public class TimeCaseController : TimeObject
         timeCaseParent.GetComponent<TMP_Text>().text = "Time Needed: " + currentPhase_i + "/" + maxTimePhase_i;
         if ( base.currentPhase_i == base.maxTimePhase_i )
         {
-            timeCaseParent.SetActive(false);
+            float rotateZ = rotationSpeed * Time.deltaTime;
+            timeCasePivotPoint.transform.Rotate(new Vector3(0f, 0f, rotateZ));
+            //Debug.Log(timeCasePivotPoint.transform.localEulerAngles.z);
+            if (timeCasePivotPoint.transform.localEulerAngles.z > 90.0f )
+            {
+                timeCasePivotPoint.SetActive(false);
+            }
         }
     }
 

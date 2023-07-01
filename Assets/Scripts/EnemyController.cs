40bf6309abcfcd96471f975724c20319e6624432
Datt Patel <dhpatel1999@gmail.com>
2023-06-30 19:46:01 -0700

    Update minor bug fix

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour, Enemy
+public class EnemyController : MonoBehaviour
 {
     public float range;
     public float speed;

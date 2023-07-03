b9de6417c68eebd9c29192bce220d884e96b3926
adam askari <34635255+bearownage@users.noreply.github.com>
2023-07-02 21:51:30 -0700

    Shop UI design, need to add controller logic

---
+++ Assets/Scripts/ShopNpcController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopNpcController : MonoBehaviour
+{
+    public Canvas shop;
+    public PlayerController player;
+
+
+    private Button closeButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

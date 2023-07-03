56cf66c6b460e14be99dc762c5af29948df66ad4
Datt Patel <dhpatel1999@gmail.com>
2023-07-03 12:41:24 -0700

    Working on Enemies/Boss/Analytics

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -6,6 +6,6 @@ public class BulletScript : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other)
     {
         gameObject.SetActive(false);
-        Destroy(this);
+        Destroy(gameObject);
     }
 }

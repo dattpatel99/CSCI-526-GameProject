5d2d4d6b02e4495bfc16d46cf0d6ab9fb3ebb385
adam askari <34635255+bearownage@users.noreply.github.com>
2023-07-03 12:19:10 -0700

    Shop Controller text, opening/closing works ish

--- Assets/Scripts/ShopNpcController.cs
+++ Assets/Scripts/ShopNpcController.cs
@@ -6,20 +6,54 @@ using UnityEngine.UI;
 public class ShopNpcController : MonoBehaviour
 {
     public Canvas shop;
+    public Canvas gameUI;
     public PlayerController player;
-
-
-    private Button closeButton;
+    public Button closeButton;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shop.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OpenShop()
+    {
+        Debug.Log("Opening shop");
+        gameUI.gameObject.SetActive(false);
+        shop.gameObject.SetActive(true);
+    }
+
+    public void CloseShop()
+    {
+        Debug.Log("Closing shop");
+        gameUI.gameObject.SetActive(true);
+        shop.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Triggering");
+        if ( collision.gameObject.name == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                OpenShop();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Triggering");
+        if (collision.gameObject.name == "Player")
+        {
+            CloseShop();
+        }
     }
 }
